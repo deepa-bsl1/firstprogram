bc70208fd0e296e701b7883c3e9a0b5bbacd8bd6
deepa-bsl1 <72477761+deepa-bsl1@users.noreply.github.com>
2020-10-09 16:25:23 -0400

    Multiplication and assignment operator problems added

---
+++ JavaGit/src/assignment.java
@@ -0,0 +1,36 @@
+
+
+public class assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=20;
+int b=5;
+int c;
+c=a+b;
+
+
+System.out.println("The assignment operator = "+c);
+
+c+=a;
+System.out.println("The assignment operator += " +c);
+b-=a;
+System.out.println("The assignment operator -= " +b);
+
+c*=a;
+System.out.println("The assignment operator *= " +c);
+	
+c/=a;
+System.out.println("The assignment operator /= " +c);
+
+c%=a;
+System.out.println("The assignment operator %= " +c);
+
+	}
+	
+	
+	
+	
+	}
+
+
---
+++ JavaGit/src/multiplication.java
@@ -0,0 +1,24 @@
+
+
+public class multiplication
+{
+
+static int a=2;
+static int b;
+static int c;
+
+public static void main(String[]args)
+{
+	
+for (b=1;b<11;b++)
+{
+	c=a*b;
+System.out.println("The multiplication of a and b is "+c);
+
+}
+
+
+
+}
+}
+
