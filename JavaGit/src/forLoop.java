e87de5e92e364c1dad62d4a27415d6e130f819e7
deepa-bsl1 <72477761+deepa-bsl1@users.noreply.github.com>
2020-10-18 20:27:44 -0400

    for loop program

---
+++ JavaGit/src/forLoop.java
@@ -0,0 +1,18 @@
+
+public class forLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		
+		for (i=100;i>0;i--)
+		{
+			System.out.println("The value of i is " +i);
+				i--;	
+			
+		}
+		
+
+	}
+
+}
